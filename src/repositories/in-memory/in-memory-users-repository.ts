a2e827fc34e03ac191f0e9c7f95154412a5992a7
Mraimundo <mdumundo18@gmail.com>
2025-10-07 15:24:55 -0300

    feat: implement in-memory user repository and registration use case
    - Add InMemoryUsersRepository for user management
    - Create RegisterUseCase to handle user registration logic
    - Implement tests for RegisterUseCase using Vitest
    - Update package.json to include Vitest and related dependencies
    - Configure Vite with tsconfig paths support

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,29 @@
+import { Prisma, User } from "generated/prisma/client";
+
+export class InMemoryUsersRepository {
+  public users: User[] = [];
+
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: `user-${Math.random()}`,
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+}
