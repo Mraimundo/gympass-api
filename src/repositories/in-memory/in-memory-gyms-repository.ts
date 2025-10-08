4b85a7640902d46c9679743281363c94788a6784
Mraimundo <mdumundo18@gmail.com>
2025-10-08 19:30:05 -0300

    feat: :sparkles: implement CreateGym use case with in-memory repository

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,38 @@
+import { Gym, Prisma } from "generated/prisma/client";
+import { randomUUID } from "node:crypto";
+
+export class InMemoryGymsRepository {
+  public gyms: Gym[] = [];
+
+  async findById(id: string) {
+    const gym = this.gyms.find((gym) => gym.id === id);
+
+    if (!gym) {
+      return null;
+    }
+
+    return gym;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude:
+        typeof data.latitude === "number"
+          ? data.latitude
+          : Number(data.latitude),
+      longitude:
+        typeof data.longitude === "number"
+          ? data.longitude
+          : Number(data.longitude),
+      created_at: new Date(),
+    };
+
+    this.gyms.push(gym);
+
+    return gym;
+  }
+}
