5b22de9ed4ce576fe0dc5efbc605afc8e413dbaa
Mraimundo <mdumundo18@gmail.com>
2025-10-09 16:15:26 -0300

    feat: :sparkles: implement FetchUserCheckInsHistory use case with pagination support

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,7 +1,7 @@
 import { CheckIn, Prisma } from "generated/prisma";
 
 export interface CheckInsRepository {
-  // countByUserId(userId: string): Promise<number>;
-  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   findByUserIdOnDate(userId: string, data: Date): Promise<CheckIn | null>;
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
 }
