efb2c7cef4cdc8ce68cd65cee56bf6073ab65a49
Mraimundo <mdumundo18@gmail.com>
2025-10-10 19:27:03 -0300

    feat: :sparkles: implement Prisma gyms repository with CRUD operations

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,49 @@
+import { prisma } from "@/lib/prisma";
+import { Gym, Prisma } from "generated/prisma";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
+
+export class PrismaUsersRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return gym;
+  }
+  async findManyNearby(params: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+      WHERE (
+        6371 * acos(
+          cos(radians(${params.latitude})) * cos(radians(latitude)) *
+          cos(radians(longitude) - radians(${params.longitude})) +
+          sin(radians(${params.latitude})) * sin(radians(latitude))
+        )
+      ) <= 10
+    `;
+
+    return gyms;
+  }
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+
+    return gyms;
+  }
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    });
+
+    return gym;
+  }
+}
