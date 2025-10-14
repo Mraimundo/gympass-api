6799c9c956b6d88fb95e130ece9390661f2d376d
Mraimundo <mdumundo18@gmail.com>
2025-10-14 16:06:10 -0300

    feat: :sparkles: Add supertest and configure e2e testing environment
    - Install supertest and @types/supertest for HTTP endpoint testing - Create custom Vitest environment for Prisma with isolated test schemas - Configure workspace-based testing with separate unit and e2e projects - Update test scripts to support both unit and e2e test execution

---
+++ prisma/vitest-evironment-prisma/prisma-test-environmant.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/prisma";
+import "dotenv/config";
+import { randomUUID } from "node:crypto";
+
+import type { Environment } from "vitest/environments";
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL env variables");
+  }
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+  return url.toString();
+}
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "ssr",
+  async setup() {
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    console.log(`Using database databaseUrl "${databaseUrl}" for testing.`);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
+      },
+    };
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,25 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  // test: {
-  //   environment: "node",
-  // },
+  test: {
+    dir: "src",
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          dir: "src/use-cases",
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "e2e",
+          dir: "src/http/controllers",
+          environment:
+            "./prisma/vitest-environment-prisma/prisma-test-environment.ts",
+        },
+      },
+    ],
+  },
 });
