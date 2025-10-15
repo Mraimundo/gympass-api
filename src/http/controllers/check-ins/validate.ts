c20cc329d382096490cc5d1430b4e4b6ec2c654d
Mraimundo <mdumundo18@gmail.com>
2025-10-15 16:28:09 -0300

    feat: :sparkles: add check-ins HTTP controllers and routes

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+import { makeValideteCheckInUseCase } from "@/use-cases/factories/make-validate-check-in-use-case";
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const validateCheckInParamsSchema = z.object({
+    checkInId: z.string().uuid(),
+  });
+
+  const { checkInId } = validateCheckInParamsSchema.parse(request.params);
+
+  const validateCheckInsUseCase = makeValideteCheckInUseCase();
+
+  await validateCheckInsUseCase.execute({
+    checkInId,
+  });
+
+  return reply.status(204).send();
+}
