e08e7d8aa353a8ea89fd133c527ce3743dfa6bd3
Mraimundo <mdumundo18@gmail.com>
2025-10-15 18:16:34 -0300

    test: :white_check_mark: add e2e tests for check-ins endpoints and fix coordinate validation

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, it, expect } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+import { prisma } from "@/lib/prisma";
+
+describe("Create Checkin (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a check-in", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: "JavaScript Gym",
+        latitude: -23.6581103,
+        longitude: -46.759936,
+      },
+    });
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        latitude: -23.6581103,
+        longitude: -46.759936,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
