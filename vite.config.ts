b1837f7241930f6dc9962f55622707f6ca98d11b
Mraimundo <mdumundo18@gmail.com>
2025-10-17 21:59:22 -0300

    refactor: :recycle: migrate from vitest workspace to projects configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,36 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+const baseConfig = defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    dir: "src",
-    workspace: [
-      {
-        extends: true,
-        test: {
-          name: "unit",
-          dir: "src/use-cases",
-        },
-      },
-      {
-        extends: true,
-        test: {
-          name: "e2e",
-          dir: "src/http/controllers",
-          environment:
-            "./prisma/vitest-environment-prisma/prisma-test-environment.ts",
-        },
-      },
-    ],
+    globals: true,
+  },
+});
+
+const unitConfig = defineConfig({
+  test: {
+    name: "unit",
+    dir: "src/use-cases",
+    environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
+  },
+});
+
+const e2eConfig = defineConfig({
+  test: {
+    name: "e2e",
+    dir: "src/http/controllers",
+    environment:
+      "./prisma/vitest-environment-prisma/prisma-test-environment.ts",
+  },
+});
+
+export default mergeConfig(baseConfig, {
+  test: {
+    projects: [unitConfig, e2eConfig],
   },
 });
