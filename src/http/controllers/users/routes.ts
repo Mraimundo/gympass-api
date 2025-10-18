74a46fbb2e4b87adfd4d139b9c1815efd008a57d
Mraimundo <mdumundo18@gmail.com>
2025-10-17 21:50:48 -0300

    refactor: :recycle: replace path aliases with relative imports in users controllers

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { profile } from "./profile";
-import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { verifyJWT } from "../../../http/middlewares/verify-jwt";
 import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
